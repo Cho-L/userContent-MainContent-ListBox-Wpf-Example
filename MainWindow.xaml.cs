6e0d6b4b971ff606c5be33ed14e46aa719218368
Cho-L <100334419+Cho-L@users.noreply.github.com>
2026-03-04 03:26:23 +0900

    first commit
    just first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LoginWpfDemo.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> People = new List<Person>
+        {
+            new Person{ Name = "아무개", Age = 23},
+            new Person{ Name = "홍길동", Age = 33},
+            new Person{ Name = "김용혁", Age = 13},
+            new Person{ Name = "주채린", Age = 24},
+            new Person{ Name = "김민서", Age = 28}
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBoxPeople.ItemsSource = People;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListBoxPeople.SelectedItems;
+            foreach (var item in selectedItems)
+            {
+                Person person = (Person)item;
+                MessageBox.Show(person.Name);
+            }
+        }
+    }
+}
